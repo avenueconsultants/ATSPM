8df267ab9deef02245ed195947d5feb8c4023e51
avetraffic <traffic@avenueconsultants.com>
2022-03-04 12:42:43 -0800

    new migration, adds 4 columns to dbo.Approach

---
+++ MOE.Common/Migrations/202203042032413_4NewAppPedColumns.cs
@@ -0,0 +1,24 @@
+﻿namespace MOE.Common.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class _4NewAppPedColumns : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Signals", "Pedsare1to1", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Approaches", "PedestrianPhaseNumber", c => c.Int());
+            AddColumn("dbo.Approaches", "IsPedestrianPhaseOverlap", c => c.Boolean(nullable: false));
+            AddColumn("dbo.Approaches", "PedestrianDetectors", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Approaches", "PedestrianDetectors");
+            DropColumn("dbo.Approaches", "IsPedestrianPhaseOverlap");
+            DropColumn("dbo.Approaches", "PedestrianPhaseNumber");
+            DropColumn("dbo.Signals", "Pedsare1to1");
+        }
+    }
+}
