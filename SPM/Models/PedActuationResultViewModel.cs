8c4605230e527a6c6c6618d9356c3ee8bdeae3dc
avetraffic <96312362+avetraffic@users.noreply.github.com>
2022-05-19 09:12:30 -0600

    pass cycles with ped call int to front end

--- SPM/Models/PedActuationResultViewModel.cs
+++ SPM/Models/PedActuationResultViewModel.cs
@@ -7,8 +7,8 @@ namespace SPM.Models
 {
     public class PedActuationResultViewModel
     {
-        public double PedActuationPercent { get; set; }
-        public int CyclesWithPedCalls { get; set; }
+        public int CyclesWithPedCallNum { get; set; }
+        public double CyclesWithPedCallPercent { get; set; }
         public bool ConsiderForStudy { get; set; }
         public Dictionary<DateTime, double> PercentCyclesWithPedsList { get; set; }
         public string Direction { get; set; }
